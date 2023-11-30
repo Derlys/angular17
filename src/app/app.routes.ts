03fa635035335a943462596b29801fb7ed88e153
Derlys Dominguez <derlys275@gmail.com>
2023-11-30 18:40:30 +0000

    add : tutorial angular 17

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {LayoutComponent} from './ui/layout/layout.component'
+import {HomeComponent} from './pages/home/home.component'
+import {BlogComponent} from './pages/blog/blog.component'
+import {ContactComponent} from './pages/contact/contact.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    // If this path is the 'full' match...
+    pathMatch: 'full',
+    // ...redirect to this route.
+    redirectTo: 'home',
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'blog', component: BlogComponent },
+      { path: 'contact', component: ContactComponent },
+    ],
+  },
+];
